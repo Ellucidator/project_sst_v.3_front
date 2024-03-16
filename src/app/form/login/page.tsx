9fb6326aeb0a3e2c415d204e2e340803e5a084af
Marcelo Leal <andradefj13@hotmail.com>
2024-03-16 15:58:59 -0300

    update
    estilização da pagina de login e funçao de validação

--- src/app/form/login/page.tsx
+++ src/app/form/login/page.tsx
@@ -1,12 +1,36 @@
+import Link from 'next/link'
 import styles from './page.module.scss'
+import { cookieService } from '@/services/cookieService';
+import { redirect } from 'next/navigation';
 
-const Login = ()=>{
+const Login =async () => {
 
-    return(
+    // const validate = await cookieService.verifySession();
+    // if(!validate){
+    //     redirect('/user/')
+    // }
+
+
+
+    return (
         <div className={styles.login}>
-            <form action="" className={`container ${styles.loginForm}`}>
-                
-            </form>
+            <div className={`container ${styles.loginContainer}`}>
+                <form action="" method='POST' className={`container ${styles.loginForm}`}>
+                    <div className={styles.inputDiv}>
+                        <label className={styles.label} htmlFor="email">Email:</label>
+                        <input type="email" name="email" id="email" className={styles.input} />
+                    </div>
+                    <div className={styles.inputDiv}>
+                        <label htmlFor="password" className={styles.label}>Senha:</label>
+                        <input type="password" name="password" id="password" className={styles.input} />
+                    </div>
+                    <button type="submit" className={styles.buttonLogin} >ENTRAR</button>
+                </form>
+                <div className={styles.divRegister}>
+                    <p className={styles.tittle}>Não tem uma conta?</p>
+                    <Link className={styles.link} href={'/form/register'}>Registre-se</Link>
+                </div>
+            </div>
         </div>
     )
 }
