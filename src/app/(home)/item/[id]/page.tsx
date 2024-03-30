bf1bb94de445872a34ec5e7920cb7c7650edc1be
Marcelo Leal <andradefj13@hotmail.com>
2024-03-30 19:45:08 -0300

    update
    teste e planejamento da pagina de items

--- src/app/(home)/item/[id]/page.tsx
+++ src/app/(home)/item/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { catalogService } from '@/services/catalogService'
 import styles from './page.module.scss'
+import Image from 'next/image'
 
 export default async function Item({ params }: { params: { id: string } }){
     const item = await catalogService.getOneItem(params.id)
@@ -13,9 +14,21 @@ export default async function Item({ params }: { params: { id: string } }){
             <div className={`container ${styles.itemContainer}`}>
                 <p>{item.name}</p>
                 <div className={styles.cardItem}>
-                    <div></div>
-                    <div className={styles.ItemBuy}></div>
+                    <div className={styles.itemBaners}>
+                        <Image 
+                        src={`http://localhost:3000/files/${item.images?.key[0]}`}
+                        alt=''
+                        width={300}
+                        height={300}
+                        />
+                    </div>
+                    <div className={styles.itemBuy}>
+
+                    </div>
                 </div>
+                <div className={styles.cep} ></div>
+                <div className={styles.itemDescription}></div>
+                <div className={styles.avaliations} ></div>
             </div>
         </div>
     )
