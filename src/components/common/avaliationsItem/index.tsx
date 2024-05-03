0d3a84da374621bd1a9e5e153421edd8cb898014
Marcelo Leal <andradefj13@hotmail.com>
2024-05-03 13:35:50 -0300

    update
    criação e estilização do componente de todas as avaliações de um item

--- src/components/common/avaliationsItem/index.tsx
+++ src/components/common/avaliationsItem/index.tsx
@@ -1,37 +1,67 @@
 import { userService } from '@/services/userService'
 import Stars from './star'
 import styles from './styles.module.scss'
+import { Avaliation } from '@/types/avaliationTypes'
+import { revalidateTag } from 'next/cache'
 
 type Props = {
-    item_id: number|string
-    user_id: number|string
+    item_id: number | string
+    user_id: number | string
+    avaliation?: Avaliation | false
+    allAvaliation?: Avaliation[]
 }
-const AvaliationsItem = async ({item_id, user_id}: Props) => {
+const AvaliationsItem = async ({ item_id, user_id, avaliation, allAvaliation }: Props) => {
 
-    const formAvaliationAction = async (form: FormData)=>{
+    const formAvaliationAction = async (form: FormData) => {
         'use server'
 
         const title = form.get('title')?.toString()
         const comment = form.get('comment')?.toString()
         const rating = form.get('stars')?.toString()
 
-        if(!title || !comment || !rating) return
+        if (!title || !comment || !rating) return
 
-        const avaliation = await userService.createAvaliation({title, comment, rating, item_id, user_id})
-        console.log(avaliation)
+        await userService.createAvaliation({ title, comment, rating, item_id, user_id })
+        revalidateTag('avaliation-user')
+        revalidateTag('all-avaliations-item')
     }
 
     return (
-        <>
-            <form className={styles.formAvaliations} action={formAvaliationAction}>
+        <section className={styles.avaliation}>
+            <p className={styles.avaliationTitleP}>SUA AVALIAÇÃO</p>
+            <div className={styles.sectionAvaliationUser}>
+                <form className={styles.formAvaliations} action={formAvaliationAction}>
 
-                <Stars />
+                    <Stars />
 
-                <input className={styles.inputTitle} type="text" name='title' placeholder='Titulo'/>
-                <textarea className={styles.inputComment} name='comment' placeholder='Comentario'/>
-                <button type="submit" >ENVIAR</button>
-            </form>
-        </>
+                    <input className={styles.inputTitle} type="text" name='title' placeholder='Titulo' />
+                    <textarea className={styles.inputComment} name='comment' placeholder='Comentario' />
+                    <button type="submit" >ENVIAR</button>
+                </form>
+                {avaliation ? (
+                    <section className={styles.avaliationUser}>
+                        <p className={styles.avaliationStars}>{avaliation.rating}</p>
+                        <p className={styles.avaliationTitle}>{avaliation.title}</p>
+                        <p className={styles.avaliationComment} >{avaliation.comment}</p>
+                    </section>
+                ) : null}
+            </div>
+            <p className={styles.avaliationTitleP}>TODAS AS AVALIAÇÕES</p>
+            <div className={styles.sectionAllAvaliations}>
+                
+                {allAvaliation ? (
+                    allAvaliation.map((avaliation,i) => {
+                        return (
+                            <div key={i + avaliation.title} className={styles.avaliationUser}>
+                                <p className={styles.avaliationStars}>{avaliation.rating}</p>
+                                <p className={styles.avaliationTitle}>{avaliation.title}</p>
+                                <p className={styles.avaliationComment} >{avaliation.comment}</p>
+                            </div>
+                        )
+                    })
+                ) : null}
+            </div>
+        </section>
     )
 }
 
