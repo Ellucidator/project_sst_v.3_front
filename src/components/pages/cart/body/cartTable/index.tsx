19d6594c42ddf1d2194de3b3539109bdb65bb3e7
Marcelo Leal <andradefj13@hotmail.com>
2024-05-06 16:36:54 -0300

    update
    criação de componente para a tabela da pag no carrinho

---
+++ src/components/pages/cart/body/cartTable/index.tsx
@@ -0,0 +1,53 @@
+import { ItemPromotion } from '@/types/itemsTypes'
+import styles from './styles.module.scss'
+import Image from 'next/image'
+
+type Props = {
+    items: ItemPromotion[]
+}
+
+const CartTable = ({ items }: Props) => {
+
+    return (
+
+        <table className={styles.tableItems}>
+            <thead>
+                <tr>
+                    <td className={styles.tableTitle}>Produtos</td>
+                    <td className={styles.tableTitle}>Qtd</td>
+                    <td className={styles.tableTitle}>Preço</td>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    items.map((item) => {
+                        return (
+                            <>
+                                <tr>
+                                    <td className='flex gap-5 '>
+                                        <Image src={`http://localhost:3000/files/${item.thumbnail_url}`} alt="banner" width={100} height={100} />
+                                        <p>{item.name}</p>
+                                    </td>
+                                    <td>{item.in_stock === 0 ? 'Indisponível' : item.quantity}</td>
+                                    <td>
+                                        {item.promotion ? (
+                                            <>
+                                                <p className={styles.pricePromotion}>{item.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+                                                <p className={styles.price}>{item.ItemPromotion.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+                                            </>
+                                        ) : (
+                                            <p className={styles.price}>{item.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+                                        )}
+                                    </td>
+                                </tr>
+                            </>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+
+    )
+}
+
+export default CartTable
