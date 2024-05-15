f39bcb9ed7edffb6c39dd17378fc2aa0906452c5
Marcelo Leal <andradefj13@hotmail.com>
2024-05-15 14:53:00 -0300

    update
    criação do typagem para endereços

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -17,3 +17,16 @@ export interface CreateUser extends Omit<User, 'id'>{
 }
 
 export interface UserPayload extends JWTPayload,User{}
+
+export interface UserAddress{
+    id: number
+    zip_code: number
+    state: string
+    city: string
+    neighborhood: string
+    street: string
+    house_number: string
+    complement?: string
+    reference_point?: string
+    active: boolean
+}
