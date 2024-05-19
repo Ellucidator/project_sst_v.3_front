2a7a0ffe6dac00cac51b3a2cbaec091654f3c402
Marcelo Leal <andradefj13@hotmail.com>
2024-05-19 15:20:38 -0300

    update
    implementação da lógica de paginação do componente de compras

--- src/components/pages/user/purchases/index.tsx
+++ src/components/pages/user/purchases/index.tsx
@@ -1,11 +1,42 @@
+'use client'
 import styles from './styles.module.scss'
 import CardPurchase from '@/components/common/cardPurchase'
+import PagCount from '@/components/common/pagCount'
 import {Purchases } from '@/types/purchaseTypes'
+import { useEffect, useState } from 'react'
 
 type Props = {
     purchases: Purchases
 }
-const UserPurchasesPage = async({purchases}:Props) => {
+const UserPurchasesPage = ({purchases}:Props) => {
+
+    const [purchasesElements, setPurchasesElements] = useState<Purchases>(purchases)
+    const [page, setPage] = useState(1)
+
+    const getPurchases = async () => {
+        const data = await fetch(`http://localhost:3000/user/show/purchases?page=${page}&perPage=${10}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhbmRyYWRlZmoxM0Bob3RtYWlsLmNvbSIsImZpcnN0X25hbWUiOiJNYXJjZWxvIiwiaW1nVXJsIjpudWxsLCJpYXQiOjE3MTYxMzk3ODYsImV4cCI6MTcxNjE2ODU4Nn0.vQdyCRqFGas7KMnI9L2_jWOT5Ez-I2I4-eXGPm0NjrM`
+            },
+            
+            cache: 'no-store',
+            next: {
+                revalidate: 10,
+                tags: ['purchases-user'],
+            },
+        })
+
+        const dataJson = await data.json()
+
+        setPurchasesElements(dataJson)
+    }
+
+    useEffect(()=>{
+        getPurchases()
+    },[page])
+
 
     return (
         <div className={styles.pageBody}>
@@ -13,13 +44,14 @@ const UserPurchasesPage = async({purchases}:Props) => {
 
             <div className={styles.userPurchasesContainer}>
                 {
-                    purchases.rows.map((elem)=>{
+                    purchasesElements.rows.map((elem)=>{
                         return(
                             <CardPurchase key={elem.id} userPurchase={elem} />
                         )
                     })
                 }
             </div>
+            <PagCount count={7} page={page} setPage={setPage} perPage={6} />
         </div>
     )
 }
