1ccd36e99309b2c8a306f743231e9c919d55fea6
Marcelo Leal <andradefj13@hotmail.com>
2024-05-20 14:00:53 -0300

    update
    Resolução de um erro que estava sendo apontado, para a possibilidade das promises serem falsas, só que antes de fazer a req já é feita uma validação do usuario

--- src/app/(home)/user/[options]/page.tsx
+++ src/app/(home)/user/[options]/page.tsx
@@ -1,6 +1,6 @@
 import { cookieService } from '@/services/cookieService'
 import styles from './page.module.scss'
-import { UserPayload } from '@/types/userTypes'
+import { UserAddress, UserPayload } from '@/types/userTypes'
 import { redirect } from 'next/navigation'
 import UserHome from '@/components/pages/user/home'
 import { userService } from '@/services/userService'
@@ -8,6 +8,7 @@ import UserAddressPage from '@/components/pages/user/address'
 import UserPurchasesPage from '@/components/pages/user/purchases'
 import FavoritesPage from '@/components/pages/user/favorites'
 import { cookies } from 'next/headers'
+import { Purchases } from '@/types/purchaseTypes'
 
 
 export default async function UserPage({params}:{params:{options:string}}) {
@@ -28,10 +29,10 @@ export default async function UserPage({params}:{params:{options:string}}) {
     return(
         <div className={styles.userInfo}>
             {
-                params.options==='home'?<UserHome user={user} userPurchase={userPurchases.rows[0]}/>:
+                params.options==='home'?<UserHome user={user} userPurchase={(userPurchases as Purchases).rows[0]}/>:
                 params.options==='my-info'?<></>:
-                params.options==='my-purchases'?<UserPurchasesPage tooken={tooken} purchases={purchases}/>:
-                params.options==='address'?<UserAddressPage userAddress={userAddress}/>:
+                params.options==='my-purchases'?<UserPurchasesPage tooken={tooken!} purchases={purchases as Purchases}/>:
+                params.options==='address'?<UserAddressPage userAddress={userAddress as UserAddress[]}/>:
                 params.options==='favorites'?<FavoritesPage/>:
                 <></>
                 }
