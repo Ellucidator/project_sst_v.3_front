ac4e2273fc060a9e80e255e189cdc991ea95fa9b
Marcelo Leal <andradefj13@hotmail.com>
2024-05-27 17:37:22 -0300

    update
    implementação do componente de input criado na pagina de informações

--- src/components/pages/user/information/index.tsx
+++ src/components/pages/user/information/index.tsx
@@ -1,14 +1,13 @@
 import { UserInfo } from '@/types/userTypes'
 import styles from './styles.module.scss'
 import { userService } from '@/services/userService'
-import Input from '@/components/common/Input-label-components/input'
-import InputAndLabel from '@/components/common/Input-label-components/input&Label'
+import Input from '@/components/common/Input-label-components/input&Label'
 
 
 
 
 const UserInformation = async () => {
-    let infoValue:UserInfo = {
+    let infoValue: UserInfo = {
         id: 0,
         first_name: '',
         last_name: '',
@@ -29,12 +28,33 @@ const UserInformation = async () => {
     }
 
     return (
-        <>  
-            <p className={styles.titleAddress}>Minhas Informações</p>
-            
-            <form action={handlerSubmit} className={styles.formAddress}>
-                <InputAndLabel labelOptions={{labelText:'teste'}} inputOptions={{placeholder:'teste'}}  />
+        <>
+            <p className={styles.title}>Minhas Informações</p>
 
+            <form action={handlerSubmit} className={styles.formInformation}>
+                <div className={styles.inputsContainer}>
+                    <Input mode='label&input' divWidth='55%' labelText={'Primeiro Nome:'}
+                        labelOptions={{ htmlFor: 'first_name' }}
+                        inputOptions={{ id: 'first_name', type: 'text', name: 'first_name', defaultValue: infoValue.first_name }} />
+
+                    <Input mode='label&input' divWidth='40%' labelText={'Sobrenome:'}
+                        labelOptions={{ htmlFor: 'last_name' }}
+                        inputOptions={{ id: 'last_name', type: 'text', name: 'last_name', defaultValue: infoValue.last_name }} />
+                </div>
+
+                <div className={styles.inputsContainer}>
+                    <Input mode='label&input' divWidth='60%' labelText={'Email:'}
+                        labelOptions={{ htmlFor: 'email' }}
+                        inputOptions={{ id: 'email', type: 'email', name: 'email', defaultValue: infoValue.email }} />
+
+                    <Input mode='label&input' divWidth='35%' labelText={'Telefone:'}
+                        labelOptions={{ htmlFor: 'phone' }}
+                        inputOptions={{ id: 'phone', type: 'text', name: 'phone', defaultValue: infoValue.phone }} />
+                </div>
+
+                <Input mode='label&input' labelText={'Data de Nascimento:'} 
+                    labelOptions={{ htmlFor: 'birth' }}
+                    inputOptions={{ id: 'birth', type: 'date', name: 'birth', defaultValue: infoValue.birth.substring(0, 10) }} />
 
                 <button className={styles.btnSave} type="submit">SALVAR</button>
             </form>
