b3f982c4cb0a9a3cc194c027a3d8a8a0231376e6
Marcelo Leal <andradefj13@hotmail.com>
2024-05-28 16:17:37 -0300

    update
    criação do middleware

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse, NextRequest } from 'next/server'
+import { cookieService } from './services/cookieService'
+import { revalidateTag } from 'next/cache'
+
+export async function middleware(request: NextRequest) {
+    
+    const verifyLogin = await cookieService.verifySession()
+    if(!verifyLogin) return NextResponse.redirect(new URL('/', request.url))
+}
+
+export const config = {
+    matcher: '/user/:path*'
+}
