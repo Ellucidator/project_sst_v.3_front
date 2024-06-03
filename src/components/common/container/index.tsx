de3c7ee2a2d44283bd46616f91069d2e9ed9ec19
Marcelo Leal <andradefj13@hotmail.com>
2024-06-03 14:00:28 -0300

    update
    mudança no componente de container  e na estilização condicional

--- src/components/common/container/index.tsx
+++ src/components/common/container/index.tsx
@@ -3,16 +3,18 @@ import styles from './styles.module.scss'
 
 type Props = {
     children: React.ReactNode
+    model?: 'model1'|'model2'
+    direction?: 'row'|'column'
     title?: string
     titleModel?: 'model1'|'model2'|'model3'|'model4'|'model5'
 }
 
-const Container = ({children,title,titleModel}:Props)=>{
+const Container = ({children,title,titleModel,model='model1',direction='row'}:Props)=>{
 
     return(
-        <div className={styles.container}>
-            {title?<Title fontSize="20px" model={titleModel!} titleText={title}/>:<></>}
-            <div className={styles.containerBody}>
+        <div className={styles.container + ' ' + styles[model]}>
+            {title?<Title fontSize="22px" model={titleModel!} titleText={title}/>:<></>}
+            <div className={styles.containerBody} style={{flexDirection:direction}}>
                 {children}
             </div>
         </div>
