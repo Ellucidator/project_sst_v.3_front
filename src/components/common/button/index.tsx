805b4a2fda907ff73570982a95dcb73357aab553
Marcelo Leal <andradefj13@hotmail.com>
2024-06-05 12:01:35 -0300

    update
    adição de uma nova condição nos botões agora aceitam um sub titulo e um icone

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -1,36 +1,46 @@
 import Link from 'next/link'
 import styles from './styles.module.scss'
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {
     btnOption?: React.ButtonHTMLAttributes<HTMLButtonElement>
     href?: string
-    btnModel: 'model1' | 'model2' | 'model3' | 'model4' 
+    btnModel: 'model1' | 'model2' | 'model3' | 'model4' | 'model5'
     btnWidth?: string
     btnName: string
-    btnAction?:'link'|'submit'|'button'
+    btnAction?: 'link' | 'submit' | 'button'
+    subTitle?: string
+    iconUrl?: string
 }
 
-const Button = ({btnOption,btnModel,btnName,btnAction='button',href,btnWidth='fit-content'}:Props)=>{
+const Button = ({ btnOption, btnModel, btnName, btnAction = 'button', href, btnWidth = 'fit-content', subTitle, iconUrl }: Props) => {
 
-    if(btnAction === 'link'){
-        return(
-            <Link style={{width:btnWidth}} href={href!} className={styles.btn + ' ' + styles[btnModel]} >
+    if (btnAction === 'link') {
+        return (
+            <Link style={{ width: btnWidth }} href={href!} className={styles.btn + ' ' + styles[btnModel]} >
                 {btnName}
             </Link>
         )
-    }else{
-        return(
+    } else {
+        return (
             <button type={btnAction}
-                style={{width:btnWidth}} 
-                className={styles.btn + ' ' + styles[btnModel]} 
+                style={{ width: btnWidth }}
+                className={styles.btn + ' ' + styles[btnModel]}
                 {...btnOption}
-                >
-                    {btnName}
-                </button>
+            >
+                {subTitle ?
+                    <div className={styles.btnTexts}>
+                        <p className={styles.title}>{btnName}</p>
+
+                        <p className={styles.subTitle}>{subTitle}</p>
+                    </div>
+                    : btnName}
+                {iconUrl?<Image src={iconUrl} alt="icon" className={styles.icon} width={35} height={35} />:<></>}
+            </button>
         )
     }
-    
+
 }
 
 
