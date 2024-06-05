3b6b0eac21cc667e31bb6a4b4db641939bc86f6e
Marcelo Leal <andradefj13@hotmail.com>
2024-06-05 11:59:21 -0300

    update
    mudança no componente loading, adição de uma condição de modelo

--- src/components/common/loading/index.tsx
+++ src/components/common/loading/index.tsx
@@ -2,13 +2,29 @@
 import styles from './styles.module.scss'
 import { useFormStatus } from "react-dom"
 
-const Loading =()=>{
-    const {pending} = useFormStatus()
-    return(
+type Props = {
+    model?: 'modelLocal'|'modelArea'
+}
+
+const Loading = ({ model = 'modelLocal' }: Props) => {
+    const { pending } = useFormStatus()
+
+    if (model === 'modelLocal')
+    return (
         <>
-            {pending?(
-                <div className={styles.loading}></div>
-            ):(<></>)}
+            {pending ? (
+                    <div className={styles.loadingLocal}></div>
+            ) : (<></>)}
+        </>
+    )
+    else if(model === 'modelArea')
+    return (
+        <>
+            {pending ? (
+                <div className={`${styles.loadingArea}`}>
+                    <div className={styles.loading}></div>
+                </div>
+            ) : (<></>)}
         </>
     )
 
