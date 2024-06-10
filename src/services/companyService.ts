b17388411b1a3f065c03299c290fe2803b33261c
Marcelo Leal <andradefj13@hotmail.com>
2024-06-10 17:10:45 -0300

    update
    estilização do footer e correção da função de req das informações da empresa

--- src/services/companyService.ts
+++ src/services/companyService.ts
@@ -1,9 +1,18 @@
-export async function getCompanyInfo() {
+import { CompanyInformation } from "@/types/companyTypes";
+
+async function getCompanyInfo() {
     const res = await fetch('http://localhost:3000/company-information',{
         method: 'GET',
-        cache: 'no-store',
+        cache: 'default',
         next: {
             revalidate: 10
         }
     })
+
+    const data: CompanyInformation = await res.json();
+    return data
+}
+
+export const companyService = {
+    getCompanyInfo
 }
