c5761e6edd71cdfca626b4adef06989387f06978
Marcelo Leal <andradefj13@hotmail.com>
2024-06-12 16:32:08 -0300

    update
    Alteração no componente de inputs para mudar a cor e direção

--- src/components/common/Input-label-components/input&Label/index.tsx
+++ src/components/common/Input-label-components/input&Label/index.tsx
@@ -3,24 +3,26 @@ import styles from './styles.module.scss'
 
 type Props = {
     mode?:'input' | 'label&input'
+    direction?:'row' | 'column'
     divWidth?: string
     inputOptions?: React.InputHTMLAttributes<HTMLInputElement>
     labelOptions?: React.LabelHTMLAttributes<HTMLLabelElement>
     labelText?: string
+    inputColor?: 'light' | 'dark'
 }
 
-const Input =({divWidth,inputOptions,labelOptions,labelText,mode}:Props)=>{
+const Input =({divWidth,inputOptions,labelOptions,labelText,mode,inputColor='light',direction='column'}:Props)=>{
 
     if(mode === 'label&input'){
         return (
-            <div className={styles.inputAndLabel} style={divWidth?{width:divWidth}:{flex:1}}>
+            <div className={styles.inputAndLabel} style={divWidth?{width:divWidth,flexDirection:direction}:{flex:1,flexDirection:direction}}>
                 <label htmlFor={inputOptions?.id} {...labelOptions} className={styles.label + ' ' + labelOptions?.className} >{labelText}</label>
-                <input id={inputOptions?.id} {...inputOptions} className={styles.input + ' ' + inputOptions?.className}  />
+                <input id={inputOptions?.id} {...inputOptions} className={styles.input + ' ' +styles[inputColor] + ' ' + inputOptions?.className}  />
             </div>
         )
     }else{
         return (
-            <input style={divWidth?{width:divWidth}:{flex:1}} {...inputOptions} className={styles.input + ' ' + inputOptions?.className}  />
+            <input style={divWidth?{width:divWidth}:{flex:1}} {...inputOptions} className={styles.input + ' ' + styles[inputColor] + ' ' + inputOptions?.className}  />
         )
     }
 }
