62a3183471246c61879b0f0296332d4b6e842456
Marcelo Leal <andradefj13@hotmail.com>
2024-06-17 17:09:14 -0300

    update
    faz parte da mudança na lógica do componente da calculadora de frete, com isso esse componente deixa de ser um cliente componente

--- src/components/common/cepCalculator/resultCepCalculator/index.tsx
+++ src/components/common/cepCalculator/resultCepCalculator/index.tsx
@@ -1,41 +1,36 @@
-'use client'
 import { CepResponse } from "@/types/cepTypes"
 import Image from "next/image"
 import styles from './styles.module.scss'
-import { useFormStatus } from "react-dom"
-import Loading from "../../loading"
-type Props = {
-    resultsCepCalculator: CepResponse[]
-}
+import { cookies } from "next/headers"
 
-const ResultCepCalculator = ({ resultsCepCalculator }: Props) => {
-    const { pending } = useFormStatus()
+
+
+const ResultCepCalculator = () => {
+    const result = cookies().get('cep-result')?.value
+    if(!result) return <></>
+
+    const resultsCepCalculator: CepResponse[] = JSON.parse(result)
+
     return (
         <>
-            {pending ? (
-                <Loading />
-            ) : (
-                <>
-                    {resultsCepCalculator.map((result) => {
-                        return (
-                            <div key={result.name} className={styles.cepResultItem}>
-                                <Image
-                                    src={result.company.picture?result.company.picture:'/public/common/ban.svg'}
-                                    alt={result.company.name}
-                                    width={result.company.picture?70:30} height={result.company.picture?70:30}
-                                    className={styles.cepResultImg}
-                                />
-                                {
-                                    result.error ?
-                                        <p>{`${result.name} - ${result.error}`}</p> :
+            {resultsCepCalculator.map((result) => {
+                return (
+                    <div key={result.name} className={styles.cepResultItem}>
+                        <Image
+                            src={result.company.picture ? result.company.picture : '/public/common/ban.svg'}
+                            alt={result.company.name}
+                            width={result.company.picture ? 70 : 30} height={result.company.picture ? 70 : 30}
+                            className={styles.cepResultImg}
+                        />
+                        {
+                            result.error ?
+                                <p>{`${result.name} - ${result.error}`}</p> :
 
-                                        <p>{`${result.name} - ${parseFloat(result.price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} - De ${result.delivery_range.min} a ${result.delivery_range.max} Dias uteis`}</p>
-                                }
-                            </div>
-                        )
-                    })}
-                </>
-            )}
+                                <p>{`${result.name} - ${parseFloat(result.price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} - De ${result.delivery_range.min} a ${result.delivery_range.max} Dias uteis`}</p>
+                        }
+                    </div>
+                )
+            })}
         </>
     )
 }
