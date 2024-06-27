bb0120b5a803681431a6ccf56a84f90a47e8960d
Marcelo Leal <andradefj13@hotmail.com>
2024-06-27 12:42:34 -0300

    update
    mudança na organização dos services, criei um novo arquivo, apenas para os serviços usados no componente de botão de ações

---
+++ src/services/btnActionService.ts
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+
+async function setCookiePage(page: string) {
+    'use server'
+    cookies().set('page', page, {
+        maxAge: 0
+    })
+}
+
+async function btnSubCategoryAction(url:string){
+    'use server'
+    const [categoryName, subCategoryId] = url.split('/')
+
+    cookies().delete(`catalog${subCategoryId}`)
+    cookies().delete(`modal`)
+
+    redirect(`/catalog/${url}`)
+}
+
+export const btnActionService = {
+
+    btnSubCategoryAction,
+    setCookiePage
+    
+}
