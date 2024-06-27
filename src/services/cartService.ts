cef75e8e0fc4b345b54567975436d4e111ceaad3
Marcelo Leal <andradefj13@hotmail.com>
2024-06-27 12:45:14 -0300

    update
    Alteração na função de add items no cart e de chamar os items que estão no carrinho, antes estavam no cookiesServices

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,6 +1,81 @@
-import { ItemToCar } from "@/types/itemsTypes"
 import { cookies } from "next/headers"
+import { ItemPromotion, ItemToCar } from "@/types/itemsTypes"
 
+
+const addCarItem = async (inStock: number, item: ItemToCar) => {
+
+    const cart = cookies().get('car')?.value
+    if (!cart) {
+        cookies().set('car', JSON.stringify([item]),
+            {
+                expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+            }
+        )
+    } else {
+        const cartItems: ItemToCar[] = JSON.parse(cart)
+        const verifyItem = cartItems.find(elem => elem.id === item.id)
+
+        if (verifyItem) {
+            verifyItem.quantity = (verifyItem.quantity + item.quantity) > inStock ? inStock : (verifyItem.quantity + item.quantity)
+        } else {
+            cartItems.push(item)
+        }
+
+        cookies().set('car', JSON.stringify(cartItems),
+            {
+                expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+            }
+        )
+    }
+
+
+
+}
+
+async function getItemsCart() {
+    const cart = cookies().get('car')?.value
+    if (!cart) return null
+
+    const cookieCart: ItemToCar[] = JSON.parse(cart)
+    const ids = cookieCart.map((item) => item.id)
+
+    const res = await fetch('http://localhost:3000/items/show-cart', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ids),
+        cache: 'no-store',
+        next: {
+            tags: ['get-items-cart']
+        }
+    })
+    const data: ItemPromotion[] = await res.json();
+
+    for (let i = 0; i < data.length; i++) {
+        const item = data.find((item) => item.id === cookieCart[i].id)
+        if (!item!.ItemCharacteristic) {
+            item!.ItemCharacteristic = {
+                id: item!.id,
+                width: 0,
+                height: 0,
+                length: 0,
+                weight: 0,
+                insurance_value: 0,
+                quantity: 0
+            }
+        }
+        if (item) {
+            if (item.in_stock < cookieCart[i].quantity) {
+                item.ItemCharacteristic!.quantity = item.in_stock
+            } else {
+                item.ItemCharacteristic!.quantity = cookieCart[i].quantity
+            }
+        }
+    }
+
+    return data
+}
 async function updateCart(value: string) {
     'use server'
     const [itemId, btnValue, inStock] = value.split('/')
@@ -28,5 +103,7 @@ async function updateCart(value: string) {
 }
 
 export const cartServices = {
-    updateCart
+    updateCart,
+    addCarItem,
+    getItemsCart
 }
