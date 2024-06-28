88efde8d7915f0aacd49ae930662daf83c88ad76
Marcelo Leal <andradefj13@hotmail.com>
2024-06-27 21:00:31 -0300

    update
    criaçãod da base e estilização da pagina de checkout

--- src/app/(home)/(conatiner)/cart/checkout/page.tsx
+++ src/app/(home)/(conatiner)/cart/checkout/page.tsx
@@ -1,10 +1,29 @@
-import Button from "@/components/common/button";
+import Title from '@/components/common/texts/tiltle'
+import styles from './page.module.scss'
+import { userService } from '@/services/userService'
+import { CardAddress } from '@/components/common/cards/cardAddress'
+import { UserAddress } from '@/types/userTypes'
+import ServerModal from '@/components/common/serverActionComponent/modal'
 
 
-export default function CheckoutPage() {
-    
+export default async function CheckoutPage() {
+
+    const adresses = await userService.getUserAdresses()
+    let addressActiv: UserAddress | undefined
+
+    if(adresses) addressActiv = adresses.find((address) => address.active === true)
+
+
     return(
-        <>
-        </>
+        <div className={styles.pageBody}>
+            <Title fontSize="25px" model='model5' titleText="Finalizar pedido" />
+            <div className={styles.addressAndPayment}>
+                <div className={styles.divAddress}>
+                    <ServerModal cookieName="address-modal" />
+                    {addressActiv?<CardAddress address={addressActiv} />:<></>}
+                </div>
+                <div className={styles.divPayment}></div>
+            </div>
+        </div>
     )
 }
