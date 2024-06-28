ed7c06e281566b64f159e389f78fa17d0a27e492
Marcelo Leal <andradefj13@hotmail.com>
2024-06-28 20:13:42 -0300

    update
    mudança no componente da pagina de endereço para que possa ser usado o mesmo corpo no modal de endereço

--- src/components/pages/user/address/index.tsx
+++ src/components/pages/user/address/index.tsx
@@ -3,13 +3,21 @@ import styles from './styles.module.scss'
 import Title from '@/components/common/texts/tiltle';
 import Button from '@/components/common/button'
 import { CardAddress } from '@/components/common/cards/cardAddress';
+import ButtonActionById from '@/components/common/serverActionComponent/buttonActionById';
+import { cookies } from 'next/headers';
 
 type Props = {
-    userAddress: UserAddress[]
+    userAddress: UserAddress[],
+    modal?:boolean
 }
-const UserAddressPage = async ({ userAddress }: Props) => {
+const UserAddressPage = async ({ userAddress, modal=false }: Props) => {
     const activeAddress = userAddress.find(address => address.active === true)
-
+    const modalAction = async(id:string)=>{
+        'use server'
+        cookies().set('modalAddressOption', id, {
+            maxAge: 0
+        })
+    }
 
     return (
         <>
@@ -17,7 +25,10 @@ const UserAddressPage = async ({ userAddress }: Props) => {
                 <Title width='100%' fontSize="25px" model='model5' titleText="Endereços" />
 
                 {userAddress.length < 6 ?
-                    <Button href="/user/edit-address/0" btnModel='model2' btnAction='link' btnName='Adicionar Endereço' /> :
+                    !modal ?
+                        <Button href="/user/edit-address/0" btnModel='model2' btnAction='link' btnName='Adicionar Endereço' /> :
+                        <ButtonActionById buttonAttribute={{ btnName: 'Adicionar Endereço', btnModel: 'model2' }} idAction={0} actionFunction={modalAction} />
+                        :
                     <></>
                 }
                 <div className={styles.divAddressList}>
@@ -31,7 +42,7 @@ const UserAddressPage = async ({ userAddress }: Props) => {
                         userAddress.map((address) => {
                             if (address.id === activeAddress?.id) return
                             return (
-                                <CardAddress address={address} buttons />
+                                <CardAddress address={address} buttons btnModal={modal} />
                             )
                         })
                     }
