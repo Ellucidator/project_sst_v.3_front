35f2779961fbe5729740d2dd036831e911188aed
Marcelo Leal <andradefj13@hotmail.com>
2024-07-01 11:12:13 -0300

    update
    alteração no componente da tabela de items, vi que a typagem ia ser diferente e criei uma condicional

--- src/components/common/itemsTable/index.tsx
+++ src/components/common/itemsTable/index.tsx
@@ -1,29 +1,54 @@
 import { Item } from '@/types/itemsTypes'
 import styles from './styles.module.scss'
 import Image from 'next/image'
+import { Purchase } from '@/types/purchaseTypes'
 
 type Props = {
-    items: Item[]
+    items: Item[] | Purchase
+    type: 'Common' | 'Purchase'
 }
-const ItemsTable = ({items}: Props) => {
+const ItemsTable = ({ items, type }: Props) => {
 
 
-    return(
-        <div className={styles.divInfo}>
-                    <div className={styles.divItems}>
-                        <p className={styles.titleItems}>Itens:</p>
-                        {items.map((elem) => {
-                            return (
-                                <div key={elem.name} className={styles.item}>
-                                    <Image src={`http://localhost:3000/files/${elem.thumbnail_url}`} alt={elem.name} width={50} height={50} />
-                                    <p>{`${elem.quantity}x ${elem.name}`}</p>
-                                    <p>{elem.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
-                                </div>
-                            )
-                        })}
-                    </div>
+    if (type === 'Common') {
+        items = items as Item[]
+
+        return (
+            <div className={styles.divInfo}>
+                <div className={styles.divItems}>
+                    <p className={styles.titleItems}>Itens:</p>
+                    {items.map((elem) => {
+                        return (
+                            <div key={elem.name} className={styles.item}>
+                                <Image src={`http://localhost:3000/files/${elem.thumbnail_url}`} alt={elem.name} width={50} height={50} />
+                                <p>{`${elem.quantity}x ${elem.name}`}</p>
+                                <p>{elem.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+                            </div>
+                        )
+                    })}
                 </div>
-    )
+            </div>
+        )
+    } else {
+        items = items as Purchase
+
+        return (
+            <div className={styles.divInfo}>
+                <div className={styles.divItems}>
+                    <p className={styles.titleItems}>Itens:</p>
+                    {items.ItemSells.map((elem) => {
+                        return (
+                            <div key={elem.Item.name} className={styles.item}>
+                                <Image src={`http://localhost:3000/files/${elem.Item.thumbnail_url}`} alt={elem.Item.name} width={50} height={50} />
+                                <p>{`${elem.quantity}x ${elem.Item.name}`}</p>
+                                <p>{elem.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
 }
 
 
