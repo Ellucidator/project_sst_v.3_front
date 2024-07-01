8e511a0fe2fc75f8d3a45bd63a8db8d87dd62ac8
Marcelo Leal <andradefj13@hotmail.com>
2024-07-01 11:46:06 -0300

    update
    Alteração no componente de preço, coloquei uma largura opcional, desta forma vai atender corretamente outros locais

--- src/components/common/texts/priceItem/index.tsx
+++ src/components/common/texts/priceItem/index.tsx
@@ -4,12 +4,15 @@ type Props = {
     price: number
     pricePromotion?: number
     model?: 'model1' | 'model2'
+    width?: string
 }
 
-export const PriceItem = ({ price, pricePromotion, model = 'model1' }: Props) => {
+export const PriceItem = ({ price, pricePromotion, model = 'model1', width }: Props) => {
 
     return (
-        <div className={`${styles.priceContainer} ${styles[model]}`}>
+        <div className={`${styles.priceContainer} ${styles[model]}`}
+            style={width?{width}:{flex:1}}
+            >
             <p className={pricePromotion? styles.pricePromotion : styles.price}>
                 {`${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`}
             </p>
