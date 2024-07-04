6fdfc1bee33258e34244046029f992acdde63d0b
Marcelo Leal <andradefj13@hotmail.com>
2024-07-04 17:04:50 -0300

    update
    implementação da lógica e feita a integração do mercado pago

--- src/app/(home)/(conatiner)/cart/checkout/page.tsx
+++ src/app/(home)/(conatiner)/cart/checkout/page.tsx
@@ -6,27 +6,50 @@ import { UserAddress } from '@/types/userTypes'
 import ServerModal from '@/components/common/serverActionComponent/modal'
 import { cartServices } from '@/services/cartService'
 import ItemsTable from '@/components/common/itemsTable'
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+import ApiMp from '@/components/common/apiMp/apiMp'
+import ButtonReturn from '@/components/common/clientOnlyComponents/btnReturn'
 
 
 export default async function CheckoutPage() {
-    const [items,total] = await cartServices.getItemsCart()
+    const client = new MercadoPagoConfig({ accessToken: 'APP_USR-1682299063331004-070414-57654f1818c2c7881d67bf58f7a58e8d-156112622' });
+    const preference = new Preference(client);
+
+    const [items, total] = await cartServices.getItemsCart()
+
+    const res = await preference.create({
+        body: {
+            items: items.map((item) => {
+                return {
+                    id: `${item.id}`,
+                    title: item.name,
+                    quantity: item.ItemCharacteristic!.quantity!,
+                    unit_price: item.promotion? item.ItemPromotion!.price: item.price,
+                }
+            })
+        }
+    })
+
     const adresses = await userService.getUserAdresses()
     let addressActiv: UserAddress | undefined
 
-    if(adresses) addressActiv = adresses.find((address) => address.active === true)
+    if (adresses) addressActiv = adresses.find((address) => address.active === true)
 
 
-    return(
+    return (
         <div className={styles.pageBody}>
             <Title fontSize="25px" model='model5' titleText="Finalizar pedido" />
             <div className={styles.addressAndPayment}>
                 <div className={styles.divAddress}>
-                    <ServerModal cookieName="modalAddress" 
-                        adresses={adresses||[]}/>
-                    {addressActiv?<CardAddress address={addressActiv} />:<></>}
+                    <ServerModal cookieName="modalAddress"
+                        adresses={adresses || []} />
+                    {addressActiv ? <CardAddress address={addressActiv} /> : <></>}
                 </div>
-                <div className={styles.divPayment}></div>
-                <ItemsTable items={items||[]} type='Common' model='model2' total={total}/>
+                <div className={styles.divPayment}>
+                    <ItemsTable items={items || []} type='Common' model='model2' total={total} />
+
+                    <ApiMp id={res.id!} />
+                </div>
             </div>
         </div>
     )
