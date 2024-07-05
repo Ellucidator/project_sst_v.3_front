2ba32f418ccf9bb82c7fce95b66408e31e9b2ede
Marcelo Leal <andradefj13@hotmail.com>
2024-07-05 16:03:22 -0300

    update
    implementação de uma chamada de api no next que faz a criação da nova compra apos o aprovamento do pagamento

---
+++ src/app/api/checkoutOk/route.ts
@@ -0,0 +1,32 @@
+import {cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+
+    const { searchParams } = new URL(request.url)
+    const token = cookies().get('token')?.value
+    if (!token) return
+
+    const address = searchParams.get('address_id')
+    
+    const cookieCart = cookies().get('car')?.value
+    
+    
+    fetch(`http://localhost:3000/user/purchase/${address}`,{
+        headers: {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: cookieCart
+    })
+    
+
+    
+    cookies().delete('car')
+
+
+    return NextResponse.redirect(`${request.nextUrl.origin}/user/home`)
+
+}
+
