1369424b1129f6656cfe33fc0887edbf41a0f1b8
Marcelo Leal <andradefj13@hotmail.com>
2024-07-08 11:28:33 -0300

    update
    criação de uma rota da api que realiza o logout, vai ficar como opção

---
+++ src/app/api/logout/route.ts
@@ -0,0 +1,12 @@
+import {cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    
+    cookies().delete('token')
+
+
+    return NextResponse.redirect(`${request.nextUrl.origin}`)
+
+}
+
