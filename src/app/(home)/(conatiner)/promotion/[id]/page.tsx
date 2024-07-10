0e22043dd767f03a345c592cb7b24df249e7405a
Marcelo Leal <andradefj13@hotmail.com>
2024-07-10 16:33:32 -0300

    update
    criação e estilização da pagina de promoções

--- src/app/(home)/(conatiner)/promotion/[id]/page.tsx
+++ src/app/(home)/(conatiner)/promotion/[id]/page.tsx
@@ -1,10 +1,61 @@
+import Container from '@/components/common/container'
 import styles from './page.module.scss'
+import Image from 'next/image'
+import { catalogService } from '@/services/catalogService'
+import CardItem from '@/components/common/cards/cardItem'
+import PagCountServer from '@/components/common/serverActionComponent/pagCount'
+import SelectOrder from '@/components/common/serverActionComponent/selectOrder'
+import { cookies } from 'next/headers'
 
 
-export default async function UserPage({params}:{params:{id:string}}) {
+export default async function PromotionPage({params}:{params:{id:string}}) {
 
+    const promotion = await catalogService.getPromotionById(params.id)
+    const subCategories = await catalogService.getSubCategories()
+
+    const actionSelect = async (form: FormData) => {
+        'use server'
+        const order = form.get('order')?.toString()!
+        const subCategoryId = form.get('subCategoryId')?.toString()!
+
+
+        const catalogCookie = cookies().get(`promotion${params.id}`)?.value
+
+
+        if (catalogCookie) {
+            const catalogCookieOn: { itemsOrder?: string, subCategoryId?: string } = JSON.parse(catalogCookie)
+
+            catalogCookieOn.itemsOrder = order
+            catalogCookieOn.subCategoryId = subCategoryId
+
+            cookies().set(`promotion${params.id}`, JSON.stringify(catalogCookieOn),
+                {
+                    maxAge: 60 * 60
+                })
+        } else {
+            cookies().set(`promotion${params.id}`, JSON.stringify({ itemsOrder: order, subCategoryId: subCategoryId }),
+                {
+                    maxAge: 60 * 60
+                })
+        }
+    }
 
     return(
-        <>teste</>
+        <div className={styles.pageContainer}>
+            <Image className={styles.pageBanner} src={`http://localhost:3000/files/${promotion.thumbnail_url}`} alt={'promotion'} width={1100} height={550} />
+            <SelectOrder title={promotion.name} formFunction={actionSelect} type='order&subCategoryId' subCategories={subCategories} />
+            <Container>
+                {   
+                    promotion.Items.length > 0?
+                    promotion.Items.map((item) => {
+                        return (
+                            <CardItem item={item} key={item.id} />
+                        )
+                    }):
+                    <></>
+                }
+            </Container>
+            <PagCountServer count={promotion.countItems||0} perPage={12}/>
+        </div>
     )
 }
