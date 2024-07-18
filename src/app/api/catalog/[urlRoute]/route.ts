aad30c6aae72f7b9556906b39472240af7b1c579
Marcelo Leal <andradefj13@hotmail.com>
2024-07-18 15:40:30 -0300

    update
    criação de uma chamada de api que substitui o form action dos links para o catalogo

---
+++ src/app/api/catalog/[urlRoute]/route.ts
@@ -0,0 +1,18 @@
+import {cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+type Params={
+    urlRoute:string
+}
+export async function GET(request: NextRequest,context:{params:Params}) {
+
+
+    const [category, subCategoryId] = context.params.urlRoute.split('-')
+
+    cookies().delete(`catalog${subCategoryId}`)
+    
+
+    return NextResponse.redirect(`http://${request.nextUrl.host}/catalog/${category}/${subCategoryId}`)
+
+}
+
