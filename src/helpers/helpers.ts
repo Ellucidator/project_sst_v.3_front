c58a84ade0cb09d2a008dd1a8b607340b8cfa677
Marcelo Leal <andradefj13@hotmail.com>
2024-07-25 17:23:39 -0300

    update
    criação de um helper para o tratamento dos cookies que são apenas números vazios, no caso o da paginação

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -7,6 +7,16 @@ function getCookieValue(cookieName: string) {
     
     return JSON.parse(cookie)
 }
+
+function getCookieIsNumber(cookieName: string): number {
+    const cookie = cookies().get(cookieName)?.value
+
+    if(cookie && typeof parseInt(cookie) === 'number')return parseInt(cookie)
+
+    return 1
+}
+
 export const helpers = {
     getCookieValue,
+    getCookieIsNumber
 }
