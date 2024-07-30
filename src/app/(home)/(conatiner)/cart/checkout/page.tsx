69ef78ab7a4e5adf591e137aabd91393372efe90
Marcelo Leal <andradefj13@hotmail.com>
2024-07-30 16:13:48 -0300

    update
    A api do mercado pago estava bugando a pagina em alguns momentos por conta dos server componentes que são controlados por cookies, decidi separar em mais uma pagina onde irá conter todas as informações finais do pedido + o pagamento

--- src/app/(home)/(conatiner)/cart/checkout/page.tsx
+++ src/app/(home)/(conatiner)/cart/checkout/page.tsx
@@ -12,59 +12,91 @@ import ButtonReturn from '@/components/common/clientOnlyComponents/btnReturn'
 import { redirect } from 'next/navigation'
 import { cepCalculatorByCart } from '@/services/cepService'
 import ResultCepCalculator from '@/components/common/cepCalculator/resultCepCalculator'
+import Button from '@/components/common/button'
+import { cookies } from 'next/headers'
+import { Cart } from '@/types/itemsTypes'
 
 
 export default async function CheckoutPage() {
     const adresses = await userService.getUserAdresses()
     let addressActiv: UserAddress | undefined
     if (adresses) addressActiv = adresses.find((address) => address.active === true)
-    
+
     const cepResponse = await cepCalculatorByCart(`${addressActiv?.zip_code!}`)
 
-    const client = new MercadoPagoConfig({ accessToken: 'APP_USR-658438204342310-070510-884ee4cac7fc572dc65f9a5c11bee043-1888685170' });
-    const preference = new Preference(client);
+    // const client = new MercadoPagoConfig({ accessToken: 'APP_USR-658438204342310-070510-884ee4cac7fc572dc65f9a5c11bee043-1888685170' });
+    // const preference = new Preference(client);
 
     const [items, total] = await cartServices.getItemsCart()
-    if(!items || items.length === 0) redirect('/')
-    
+    if (!items || items.length === 0) redirect('/')
 
-    const res = await preference.create({
-        body: {
-            items: items.map((item) => {
-                return {
-                    id: `${item.id}`,
-                    title: item.name,
-                    quantity: item.ItemCharacteristic!.quantity!,
-                    unit_price: item.promotion? item.ItemPromotion!.price: item.price,
-                }
-            }),
-            back_urls:{
-                success: `http://localhost:3001/api/checkoutOk?address_id=${addressActiv?.id}`,
-                pending: `http://localhost:3001/api/checkoutOk?address_id=${addressActiv?.id}`,
-            },
-            auto_return: 'approved'
-        }
-    })    
 
+    // const res = await preference.create({
+    //     body: {
+    //         items: items.map((item) => {
+    //             return {
+    //                 id: `${item.id}`,
+    //                 title: item.name,
+    //                 quantity: item.ItemCharacteristic!.quantity!,
+    //                 unit_price: item.promotion ? item.ItemPromotion!.price : item.price,
+    //             }
+    //         }),
+    //         back_urls: {
+    //             success: `http://localhost:3001/api/checkoutOk?address_id=${addressActiv?.id}`,
+    //             pending: `http://localhost:3001/api/checkoutOk?address_id=${addressActiv?.id}`,
+    //         },
+    //         auto_return: 'approved'
+    //     }
+    // })
+
+const handlerSubmit = async (form: FormData) => {
+    'use server'
+    if(!addressActiv) return
+
+    const frete = form.get('cep-result')?.toString()
+    const [service, price] = frete!.split('-')
+    if(!price) return
+
+    const cartCookie = cookies().get('car')?.value
+
+    const cart:Cart = JSON.parse(cartCookie!)
+
+    cart.frete = {
+        address_id: addressActiv.id!,
+        name: service,
+        price: parseFloat(price)
+    }
+    cookies().set('car', JSON.stringify(cart))
+
+    redirect('/cart/payment')
+}
 
     return (
-        <div className={styles.pageBody}>
+        <form action={handlerSubmit} className={styles.pageBody}>
             <Title fontSize="25px" model='model5' titleText="Finalizar pedido" />
             <div className={styles.addressAndPayment}>
-                <div className={styles.divAddress}>
-                    <ServerModal cookieName="modalAddress"
-                        adresses={adresses || []} />
-                    {addressActiv ? <CardAddress address={addressActiv} /> : <></>}
+                <div className={styles.divAddressAndFrete}>
+                    <div className={styles.divAddress}>
+                        <ServerModal cookieName="modalAddress"
+                            adresses={adresses || []} />
+                        {addressActiv ? <CardAddress address={addressActiv} /> : <></>}
+                    </div>
+                    <div className={styles.divFrete}>
+                        <Title fontSize="25px" model='model5' width='100%' titleText="Metodo de envio:" />
+                        <ResultCepCalculator type='form' resultCepCalc={cepResponse} />
+                    </div>
                 </div>
                 <div className={styles.divPayment}>
-                    <div className={styles.divFrete}>
-                        <ResultCepCalculator type='form' resultCepCalc={cepResponse}/>
-                    </div>
-                    <ItemsTable items={items || []} type='Common' model='model2' total={total} />
 
+                    <ItemsTable items={items || []} type='Common' model='model2' total={total} />
+                    <Button btnName='Prosseguir para pagamento' btnAction='submit' btnWidth='fit-content'
+                            btnModel='model2' btnOption={{style:{fontSize:'x-large'}}}
+                            iconElem={{src:'/public/common/cash-stack.svg',width:30,position:'right'}}
+
+                    />
                     {/* {addressActiv?<ApiMp id={res.id!} />:<></>} */}
                 </div>
             </div>
-        </div>
+        </form>
     )
 }
