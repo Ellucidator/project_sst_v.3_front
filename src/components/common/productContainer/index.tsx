59a810062fbfe0a9ae561a1724bb66e9058510a0
Marcelo Leal <andradefj13@hotmail.com>
2024-08-19 14:38:59 -0300

    update
    criação de um componente de contêiner já com tratamento

---
+++ src/components/common/productContainer/index.tsx
@@ -0,0 +1,21 @@
+import { Item, ItemPromotion } from "@/types/itemsTypes"
+import Container, { ContainerAttributes } from "../container"
+import CardItem from "../cards/cardItem"
+
+type Props = {
+    containerAttributes: Omit<ContainerAttributes, 'children'>
+    products?:Item[]|ItemPromotion[]
+}
+const ProductContainer = ({containerAttributes,products}:Props) => {
+    if(!products)return<></>
+
+    return (
+        <Container {...containerAttributes}>
+            {products.map((elem) => {
+                return <CardItem item={elem} key={elem.id} />
+            })}
+        </Container>
+    )
+}
+
+export default ProductContainer
