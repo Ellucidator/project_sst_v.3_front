ac259c44880c2d94dea4540e5a154af742581d97
Marcelo Leal <andradefj13@hotmail.com>
2024-08-26 14:50:35 -0300

    update
    teste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,25 +12,26 @@ export const metadata: Metadata = {
   alternates: {
     canonical:"/"
   },
-  openGraph: {
-    title: "Ségio Setup",
-    description: "Ségio Setup e-commerce",
-    url: "https://segio-setup.vercel.app/",
-    siteName: "Ségio Setup",
-    images:{
-        url: "https://segio-setup.vercel.app/public/home/icon.png",
-        width: 1920,
-        height: 1080,
-      },
-    type: "website",
-  },
-  twitter: {
-    title: "Ségio Setup",
-    description: "Ségio Setup e-commerce",
-    card: "summary_large_image",
-    images: "https://segio-setup.vercel.app/public/home/icon.png",
-    creator: "@SegioSetup",
-  }
+  // openGraph: {
+  //   title: "Ségio Setup",
+  //   description: "Ségio Setup e-commerce",
+  //   url: "https://segio-setup.vercel.app/",
+  //   siteName: "Ségio Setup",
+  //   images:{
+  //       url: "https://segio-setup.vercel.app/public/home/icon.png",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //   type: "website",
+  // },
+  // twitter: {
+  //   title: "Ségio Setup",
+  //   description: "Ségio Setup e-commerce",
+  //   card: "summary_large_image",
+  //   images: "https://segio-setup.vercel.app/public/home/icon.png",
+  //   creator: "@SegioSetup",
+  // },
+
 };
 
 export default function RootLayout({
