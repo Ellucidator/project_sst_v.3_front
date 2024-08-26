e58e7aa085d34f25cde0d162279e15b0441836b6
Marcelo Leal <andradefj13@hotmail.com>
2024-08-26 14:45:27 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,28 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ségio Setup",
   description: "Ségio Setup e-commerce",
-  verification: {
-    google: "AIzaSyCQW8k7DZnqoY4gqz3fGKqgqY0r5I8cBp0",
+  metadataBase: new URL("https://segio-setup.vercel.app"),
+  alternates: {
+    canonical:"/"
+  },
+  openGraph: {
+    title: "Ségio Setup",
+    description: "Ségio Setup e-commerce",
+    url: "https://segio-setup.vercel.app/",
+    siteName: "Ségio Setup",
+    images:{
+        url: "https://segio-setup.vercel.app/public/home/icon.png",
+        width: 1920,
+        height: 1080,
+      },
+    type: "website",
+  },
+  twitter: {
+    title: "Ségio Setup",
+    description: "Ségio Setup e-commerce",
+    card: "summary_large_image",
+    images: "https://segio-setup.vercel.app/public/home/icon.png",
+    creator: "@SegioSetup",
   }
 };
 
