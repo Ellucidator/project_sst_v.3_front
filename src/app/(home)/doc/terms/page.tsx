1e1ad38a8555e3cd47b40bf7a51f26e16d33ef5e
Marcelo Leal <andradefj13@hotmail.com>
2024-09-25 13:01:24 -0300

    #feat
    implementação dos termos e serviços

---
+++ src/app/(home)/doc/terms/page.tsx
@@ -0,0 +1,63 @@
+export default async function Privacy() {
+
+
+
+
+    return (
+        <>
+            <div style={{ padding: '2rem',backgroundColor: 'white' }}>
+
+                <h2 style={{ color: 'rgb(68, 68, 68)' }}>1. Termos</h2>
+                <p style={{ color: 'rgb(68, 68, 68)' }}>
+                    Ao acessar ao site <a href="https://project-sst-v-3-front.vercel.app/">e-commerce</a>, concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis ​​e concorda que é responsável pelo cumprimento de todas as leis locais aplicáveis. Se você não concordar com algum desses termos, está proibido de usar ou acessar este site. Os materiais contidos neste site são protegidos pelas leis de direitos autorais e marcas comerciais aplicáveis.
+                </p>
+
+                <h2 style={{ color: 'rgb(68, 68, 68)' }}>2. Uso de Licença</h2>
+                <p style={{ color: 'rgb(68, 68, 68)' }}>
+                    É concedida permissão para baixar temporariamente uma cópia dos materiais (informações ou software) no site e-commerce, apenas para visualização transitória pessoal e não comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob esta licença, você não pode:
+                </p>
+                <ol style={{ color: 'rgb(68, 68, 68)' }}>
+                    <li style={{ color: 'rgb(68, 68, 68)' }}>modificar ou copiar os materiais;</li>
+                    <li style={{ color: 'rgb(68, 68, 68)' }}>usar os materiais para qualquer finalidade comercial ou para exibição pública (comercial ou não comercial);</li>
+                    <li style={{ color: 'rgb(68, 68, 68)' }}>tentar descompilar ou fazer engenharia reversa de qualquer software contido no site e-commerce;</li>
+                    <li style={{ color: 'rgb(68, 68, 68)' }}>remover quaisquer direitos autorais ou outras notações de propriedade dos materiais; ou</li>
+                    <li style={{ color: 'rgb(68, 68, 68)' }}>transferir os materiais para outra pessoa ou 'espelhar' os materiais em qualquer outro servidor.</li>
+                </ol>
+                <p style={{ color: 'rgb(68, 68, 68)' }}>
+                    Esta licença será automaticamente rescindida se você violar alguma dessas restrições e poderá ser rescindida por e-commerce a qualquer momento. Ao encerrar a visualização desses materiais ou após o término desta licença, você deve apagar todos os materiais baixados em sua posse, seja em formato eletrónico ou impresso.
+                </p>
+
+                <h2 style={{ color: 'rgb(68, 68, 68)' }}>3. Isenção de responsabilidade</h2>
+                <ol style={{ color: 'rgb(68, 68, 68)' }}>
+                    <li style={{ color: 'rgb(68, 68, 68)' }}>Os materiais no site da e-commerce são fornecidos 'como estão'. e-commerce não oferece garantias, expressas ou implícitas, e, por este meio, isenta e nega todas as outras garantias, incluindo, sem limitação, garantias implícitas ou condições de comercialização, adequação a um fim específico ou não violação de propriedade intelectual ou outra violação de direitos.</li>
+                    <li style={{ color: 'rgb(68, 68, 68)' }}>Além disso, o e-commerce não garante ou faz qualquer representação relativa à precisão, aos resultados prováveis ou à confiabilidade do uso dos materiais em seu site ou de outra forma relacionado a esses materiais ou em sites vinculados a este site.</li>
+                </ol>
+
+                <h2 style={{ color: 'rgb(68, 68, 68)' }}>4. Limitações</h2>
+                <p style={{ color: 'rgb(68, 68, 68)' }}>
+                    Em nenhum caso o e-commerce ou seus fornecedores serão responsáveis ​​por quaisquer danos (incluindo, sem limitação, danos por perda de dados ou lucro ou devido a interrupção dos negócios) decorrentes do uso ou da incapacidade de usar os materiais em e-commerce, mesmo que e-commerce ou um representante autorizado da e-commerce tenha sido notificado oralmente ou por escrito da possibilidade de tais danos. Como algumas jurisdições não permitem limitações em garantias implícitas, ou limitações de responsabilidade por danos consequentes ou incidentais, essas limitações podem não se aplicar a você.
+                </p>
+
+                <h2 style={{ color: 'rgb(68, 68, 68)' }}>5. Precisão dos materiais</h2>
+                <p style={{ color: 'rgb(68, 68, 68)' }}>
+                    Os materiais exibidos no site da e-commerce podem incluir erros técnicos, tipográficos ou fotográficos. e-commerce não garante que qualquer material em seu site seja preciso, completo ou atual. e-commerce pode fazer alterações nos materiais contidos em seu site a qualquer momento, sem aviso prévio. No entanto, e-commerce não se compromete a atualizar os materiais.
+                </p>
+
+                <h2 style={{ color: 'rgb(68, 68, 68)' }}>6. Links</h2>
+                <p style={{ color: 'rgb(68, 68, 68)' }}>
+                    O e-commerce não analisou todos os sites vinculados ao seu site e não é responsável pelo conteúdo de nenhum site vinculado. A inclusão de qualquer link não implica endosso por e-commerce do site. O uso de qualquer site vinculado é por conta e risco do usuário.
+                </p>
+
+                <h3 style={{ color: 'rgb(68, 68, 68)' }}>Modificações</h3>
+                <p style={{ color: 'rgb(68, 68, 68)' }}>
+                    O e-commerce pode revisar estes termos de serviço do site a qualquer momento, sem aviso prévio. Ao usar este site, você concorda em ficar vinculado à versão atual desses termos de serviço.
+                </p>
+
+                <h3 style={{ color: 'rgb(68, 68, 68)' }}>Lei aplicável</h3>
+                <p style={{ color: 'rgb(68, 68, 68)' }}>
+                    Estes termos e condições são regidos e interpretados de acordo com as leis do e-commerce e você se submete irrevogavelmente à jurisdição exclusiva dos tribunais naquele estado ou localidade.
+                </p>
+            </div>
+        </>
+    )
+}
