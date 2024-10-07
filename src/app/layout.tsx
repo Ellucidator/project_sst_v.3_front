e22b52e63ea76f931a9decf6b25f8703c50740fa
Marcelo Leal <andradefj13@hotmail.com>
2024-10-07 14:53:53 -0300

    #test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Tomorrow } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import Footer from "@/components/footer";
 
 const tomorrow = Tomorrow({ subsets: ["latin"], weight: ["400"] });
@@ -18,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <GoogleTagManager gtmId="GTM-WHM7BCKV" />
+      <GoogleAnalytics gaId="G-T9X570YDPN" />
+
       <body className={` ${tomorrow.className}`}>
         {children}
         <Footer />
